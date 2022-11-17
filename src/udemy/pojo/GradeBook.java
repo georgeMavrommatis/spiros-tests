6ea0108a7af77039315653b2065f15649192a0b4
Spiros Mavrommatis <SpirosMavrommatis91@gmail.com>
2022-11-17 12:38:03 +0200

    Comments about GradeBook & GradeBookTest

--- src/udemy/pojo/GradeBook.java
+++ src/udemy/pojo/GradeBook.java
@@ -1,5 +1,5 @@
 package udemy.pojo;
-//ΔΗΛΩΣΗ ΚΛΑΣΗΣ ΚΑΙ ΜΕΘΟΔΟΥ
+//ΔΗΛΩΣΗ ΚΛΑΣΗΣ ΚΑΙ ΜΕΘΟΔΟΥ ΜΕ ΠΑΡΑΜΕΤΡΟ
 /*Δεν έχουμε τη βασική μέθοδο εκτέλεσης του
 προγράμματος, ωστόσο έχουμε την GradebookTest
 κλάση(αρχείο), εντός της οποίας υπάρχει η main
@@ -8,7 +8,12 @@ package udemy.pojo;
 τη μέθοδο displayMessage*/
 
 public class GradeBook {
-    public void displayMessage(){
-        System.out.println("Welcome to the GradeBook!");
+    public void displayMessage(String courseName){
+        /* Η μέθοδος displayMessage εμπεριέχει τον
+        access modifier, τον type of redundant return
+        και την παράμετρο της μεθόδου */
+
+        System.out.println("Welcome to the grade book for " + courseName);
+        /*Το όρισμα αυτό θα μεταβληθεί*/
     }
 }
--- src/udemy/pojo/GradeBookTest.java
+++ src/udemy/pojo/GradeBookTest.java
@@ -1,7 +1,12 @@
 package udemy.pojo;
+//ΕΛΕΓΧΟΣ ΚΛΑΣΗΣ
+import java.util.Scanner;
 
 public class GradeBookTest {
     public static void main(String[] args){
+
+        Scanner input = new Scanner (System.in);
+
         GradeBook myGradeBook = new GradeBook();
         /* Η μεταβλητή myGradeBook εκκινείται-αρχικοποιείται
          από τη στιγμιοτυπική έκφραση για τη δημιουργία
@@ -9,12 +14,19 @@ public class GradeBookTest {
          Η λέξη-κλειδί new δημιουργεί το αντικείμενο Gradebook
          με έναν () κονστράκτορα, εντός του οποίου καθορίζονται
          τα δεδομένα του αντικειμένου*/
+        System.out.println("Please enter the name of the course");
+        String nameOfCourse = input.nextLine();
+        System.out.println();
 
-        myGradeBook.displayMessage();
+        myGradeBook.displayMessage(nameOfCourse);
         /* Ακριβώς όπως μπορούμε να καλέσουμε τις μεθόδους
         print (σκέτο, f, ln), χρησιμοποιώντας την προκαθορισμένη
         κλάση System και τον τελεστή της τελείας που καλεί την
         ιδιότητα out, έτσι κάνουμε και με τη μεταβλητή
-        που δημιουργήσαμε στην προηγούμενη κλάση*/
+        που δημιουργήσαμε στην προηγούμενη κλάση. Στην
+        προκειμένη περίπτωση 1)ο κώδικας της γραμμής 21
+        καλεί τη μέθοδο displayMessage της μεταβλητής myGradeBook
+        και η μεταβλητή nameOfCourse γίνεται τιμή-όρισμα της παραμέτρου
+        courseName της μεθόδου displayName*/
     }
 }
