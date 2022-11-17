59277011cbb03a913bb4f75d84e80e2298d704bd
georgeMavrommatis <66722238+georgeMavrommatis@users.noreply.github.com>
2022-11-17 14:49:10 +0200

    Update GradeBook.java
    Added javadoc basic intro

--- src/udemy/pojo/GradeBook.java
+++ src/udemy/pojo/GradeBook.java
@@ -7,7 +7,13 @@ package udemy.pojo;
 που θα ελέγξει την εκτέλεση) και τη μεταβλητή
 της έκφρασης στιγμιότυπου που θα καλέσει
 τη μέθοδο displayMessage*/
-
+/**
+*<pre>
+* This is Javadoc
+* This is a link <a href="https://www.oracle.com/uk/technical-resources/articles/java/javadoc-tool.html">Link<a/>
+*</pre>
+* @author Your Name
+*/
 public class GradeBook {
     public void displayMessage(String courseName){
         /*Η μέθοδος displayMessage εμπεριέχει τον
